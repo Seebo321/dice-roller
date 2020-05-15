5dca0e9e05e5c1da81d1a9f1a65d87db4fd07669
Samuel Campbell <seebo321@comcast.net>
2020-05-15 15:12:42 -0500

    updated javascript and added sides to dyce

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,9 +5,9 @@ const total = document.querySelector('#total')
 const diceRollsButton = document.querySelector('#diceRolls')
 const resetButton = document.querySelector('#reset')
 const diceList = document.querySelector('#dicelist')
+const sides = document.querySelector('#sides')
+let sidesValue = sides.value
 let totalAmount = 0
-
-
 let diceRolls =[]
 let index=0
 let lists = []
@@ -15,17 +15,17 @@ let diceRollslength=diceRolls.length
 let number =('<li>' + diceRolls[0] + '</li>')
 
 
-
+// functions
 
 rollButton.addEventListener("click", function () {  
     let diceNumberValue= diceNumber.value;
     let rollNumber = 0
     diceRolls =[]
     lists = []
+    let a=sides.value
    while (rollNumber < diceNumberValue){ 
        rollNumber ++;
-   console.log(diceNumberValue)
-   let y =  Math.floor(Math.random() *6)+1;
+   let y =  Math.floor(Math.random() *a)+1;
    console.log(y)
    diceRolls.push(y)
    console.log(diceRolls)
@@ -59,4 +59,7 @@ resetButton.addEventListener("click", function(){
     diceNumber.value = 0
     total.innerHTML = ('')
     totalAmount = 0
+    sides.value = ('6')
 })
+
+
